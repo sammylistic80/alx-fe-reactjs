5372e13e9d6411dce6564a35f1c9a830d5b5bd1f
Enyi Samuel Onyekachi <enyisamuel80@gmail.com>
2025-02-22 17:30:29 +0100

    add initial React components and Vite configuration

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{backgroundColor: 'lightgray', color: 'blue', textAlign: 'center'}}>
+            <p style={{color: 'black'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
