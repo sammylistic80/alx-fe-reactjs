50297ccbba77ec8b354cc641aef9c313db7a4144
Enyi Samuel Onyekachi <enyisamuel80@gmail.com>
2025-02-22 19:12:46 +0100

    refactor UserContext: provide default values and update imports in App and UserProfile

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,4 +1,5 @@
-import { UserContext } from "./components/UserContext"; import React, { useState } from 'react';
+import { UserContext } from "./components/UserContext";
+import React, { useState } from 'react';
 import UserProfile from './components/UserProfile';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -1,3 +1,9 @@
 import { createContext } from "react";
 
-export const UserContext = createContext({});
+// Provide a default object to avoid null errors
+export const UserContext = createContext({
+  name: "Guest",
+  email: "guest@example.com",
+});
+
+export default UserContext;
