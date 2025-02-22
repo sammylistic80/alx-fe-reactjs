50ed14a2980be5e1ea092c8c839843e02fdd0152
Enyi Samuel Onyekachi <enyisamuel80@gmail.com>
2025-02-22 19:03:58 +0100

    refactor UserContext: move to components directory and update imports in UserProfile, UserDetails, and App

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { UserContext } from "./contexts/UserContext";
+import { UserContext } from "./UserContext";
 
 function UserProfile() {
   const userData = useContext(UserContext);
