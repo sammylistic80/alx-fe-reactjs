25581ae9ddb7e8e10a1c0109b3f9e7072183542b
Enyi Samuel Onyekachi <enyisamuel80@gmail.com>
2025-02-24 18:02:33 +0100

    add initial project structure with components, styles, and assets

---
+++ my-company/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import './App.css';
+import index from './index.css';
+import './Home.css';
+
+function App() {
+  return (
+    <Router>
+        <Navbar />
+      <Routes>
+        <Route path="/App" element={<App />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
