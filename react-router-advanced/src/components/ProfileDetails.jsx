89d5554b37c3c77709b08ecf3c72742dea2874fd
sammylistic80 <enyisamuel80@gmail.com>
2025-03-08 18:26:30 +0100

    feat: initialize React app with Vite and add Profile components

---
+++ react-router-advanced/src/components/ProfileDetails.jsx
@@ -0,0 +1,6 @@
+const ProfileDetails = () => {
+    return <h3>Profile Details Component</h3>;
+  };
+  
+  export default ProfileDetails;
+  
---
+++ react-router-advanced/src/components/ProfileSettings.jsx
@@ -0,0 +1,6 @@
+const ProfileSettings = () => {
+    return <h3>Profile Settings Component</h3>;
+  };
+  
+  export default ProfileSettings;
+  
