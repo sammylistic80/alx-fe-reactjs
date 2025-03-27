f11d8db0050b262e7ea2484c793efa054e2ef8fe
sammylistic80 <enyisamuel80@gmail.com>
2025-03-27 14:29:33 +0100

    Done

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import UserProfile from './UserProfile'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <>
+    <UserProfile />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
